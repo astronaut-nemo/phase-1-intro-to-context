967f89e2cde7275b56b5b90e6ddb266a824e0575
Joanna <astronautnemo@gmail.com>
2022-12-08 14:35:58 +0300

    completed deliverables 1 and 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function createEmployeeRecord(array){
+    const employeeRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
+
+function createEmployeeRecords(arrayOfArrays){
+    const arrayOfObjects = [];
+    arrayOfArrays.map(nestedArray => {arrayOfObjects.push(createEmployeeRecord(nestedArray))});
+    
+    return arrayOfObjects;
+}
+
+let twoRows = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+  ]
+
+console.log(createEmployeeRecords(twoRows))
