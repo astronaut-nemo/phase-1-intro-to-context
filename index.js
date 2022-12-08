d8110c194759440f6c6cd1b7e4897ebc009218c2
Joanna <astronautnemo@gmail.com>
2022-12-08 15:08:41 +0300

    completed deliverables 3 and 4

--- index.js
+++ index.js
@@ -14,13 +14,34 @@ function createEmployeeRecord(array){
 function createEmployeeRecords(arrayOfArrays){
     const arrayOfObjects = [];
     arrayOfArrays.map(nestedArray => {arrayOfObjects.push(createEmployeeRecord(nestedArray))});
-    
+
     return arrayOfObjects;
 }
 
-let twoRows = [
-    ["moe", "sizlak", "barkeep", 2],
-    ["bartholomew", "simpson", "scamp", 3]
-  ]
+function createTimeInEvent(employeeRecord, dateStamp){
+    employeeRecord.timeInEvents[0] = {
+        type: "TimeIn",
+        hour: Number.parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0, 10)
+    }
+    return employeeRecord
+}
 
-console.log(createEmployeeRecords(twoRows))
+function createTimeOutEvent(employeeRecord, dateStamp){
+    employeeRecord.timeOutEvents[0] = {
+        type: "TimeOut",
+        hour: Number.parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0, 10)
+    }
+    return employeeRecord
+}
+
+function hoursWorkedOnDate(employeeRecord, date){
+    
+}
+
+let employeeDetails = ['Shouto', 'Todoroki', 'Fridge', 10]
+let employeeRecord = createEmployeeRecord(employeeDetails)
+createTimeInEvent(employeeRecord, "2014-02-28 1400")
+createTimeOutEvent(employeeRecord, "2014-02-28 1700")
+console.log(employeeRecord)
