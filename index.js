62ea4c2f15a3f836846e230f049a8350e821203b
Joanna <astronautnemo@gmail.com>
2022-12-08 20:46:21 +0300

    t
    completed assignment

--- index.js
+++ index.js
@@ -19,29 +19,85 @@ function createEmployeeRecords(arrayOfArrays){
 }
 
 function createTimeInEvent(employeeRecord, dateStamp){
-    employeeRecord.timeInEvents[0] = {
+    employeeRecord.timeInEvents.push({
         type: "TimeIn",
         hour: Number.parseInt(dateStamp.slice(11)),
         date: dateStamp.slice(0, 10)
-    }
+    })
     return employeeRecord
 }
 
 function createTimeOutEvent(employeeRecord, dateStamp){
-    employeeRecord.timeOutEvents[0] = {
+    employeeRecord.timeOutEvents.push({
         type: "TimeOut",
         hour: Number.parseInt(dateStamp.slice(11)),
         date: dateStamp.slice(0, 10)
-    }
+    })
     return employeeRecord
 }
 
 function hoursWorkedOnDate(employeeRecord, date){
-    
+    for(let i in employeeRecord.timeInEvents){
+        if(employeeRecord.timeInEvents[i].date === date){
+            return (employeeRecord.timeOutEvents[i].hour - employeeRecord.timeInEvents[i].hour)/100
+        }
+    }
+    // // Check that the date is the same as the time in
+    // const startTime = (date === employeeRecord.timeInEvents[0].date ? employeeRecord.timeInEvents[0].hour : console.log('Different date than time in.'));
+    // // Check that the date is the same as the time out
+    // const endTime = (date === employeeRecord.timeInEvents[0].date ? employeeRecord.timeOutEvents[0].hour : console.log('Different date than time out.'));
+
+    // const hoursWorked = (endTime - startTime)/100;
+    // return hoursWorked;
 }
 
-let employeeDetails = ['Shouto', 'Todoroki', 'Fridge', 10]
-let employeeRecord = createEmployeeRecord(employeeDetails)
-createTimeInEvent(employeeRecord, "2014-02-28 1400")
-createTimeOutEvent(employeeRecord, "2014-02-28 1700")
-console.log(employeeRecord)
+function wagesEarnedOnDate(employeeRecord, date){
+    const payOwed = hoursWorkedOnDate(employeeRecord, date) * employeeRecord.payPerHour;
+    return payOwed;
+}
+
+function allWagesFor(employeeRecord){
+    // use wagesEarnedOnDate to get the wages for that day then add it to the accumulated value (reduce) for each timeInEvent element   
+    let total = employeeRecord.timeInEvents.reduce((accumulator, timeInElement) => {
+        let total = wagesEarnedOnDate(employeeRecord, timeInElement.date)
+        return accumulator += total;
+    }, 0)
+    return total;
+}
+
+function calculatePayroll(arrayOfEmployees){
+    const reducer = (accumulator, employee) => {
+        let totalWages = allWagesFor(employee);
+        return accumulator += totalWages;
+    }
+   return arrayOfEmployees.reduce(reducer, 0)
+}
+
+let rRecord = createEmployeeRecord(["Rafiki", "", "Aide", 10])
+let sRecord = createEmployeeRecord(["Simba", "", "King", 100])
+
+let sTimeData = [
+    ["2019-01-01 0900", "2019-01-01 1300"], // 4 * 100 = 400
+    ["2019-01-02 1000", "2019-01-02 1300"]  // 3 * 100 = 300 ===> 700 total
+]
+
+let rTimeData = [
+    ["2019-01-11 0900", "2019-01-11 1300"], // 4 * 10 = 40
+    ["2019-01-12 1000", "2019-01-12 1300"]  // 3 * 10 = 40 ===> 70 total ||=> 770
+]
+
+sTimeData.forEach(function (d) {
+    let [dIn, dOut] = d
+    sRecord = createTimeInEvent(sRecord, dIn)
+    sRecord = createTimeOutEvent(sRecord, dOut)
+})
+
+rTimeData.forEach(function (d, i) {
+    let [dIn, dOut] = d
+    rRecord = createTimeInEvent(rRecord, dIn)
+    rRecord = createTimeOutEvent(rRecord, dOut)
+})
+
+let employees = [sRecord, rRecord]
+
+console.log(calculatePayRoll(employees))
